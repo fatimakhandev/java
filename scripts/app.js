3380a9bb0557003b01a9cd8177cc10e78ba78bdc
fatimakhandev <fatimakhan0937@outlook.com>
2023-04-09 08:21:04 +0500

    Initialize Project

---
+++ scripts/app.js
@@ -0,0 +1,119 @@
+// 1st Task( 1st day) Date 06/04/2023
+
+// chapter 1st "Alerts"
+alert ("Thanks for your input!");
+alert ( "Thanks for your input!" );
+
+// chapter 2nd "Variables for Strings"
+var name = "Mark";
+var name = "Mark";
+name = "Ace";
+var nationality;
+nationality = "U.S.";
+var floogle = "Mark";
+var name = "Mark";
+var floogle = "Mark";
+var lessonAuthor = "Mark";
+var guyWhoKeepsSayingHisOwnName = "Mark";
+var x = "Mark";
+var lastName = "Smith";
+var cityOfOrigin = "New Orleans";
+var aussieGreeting = "g'Day";
+var nickname = "Bub";
+alert("Thanks for your input!");
+1 var thanx = "Thanks for your input!"
+2 alert(thanx);
+
+// chapter 3rd "Variables for Numbers"
+var weight = 150;
+weight + 25
+1 var originalNum = 23;
+2 var newNum = originalNum + 7;
+1 var originalNum = 23;
+2 var numToBeAdded = 7;
+3 var newNum = originalNum + numToBeAdded;
+1 var originalNum = 90;
+2 originalNum = originalNum + 10;
+1 var originalNum = "23";
+2 var newNum = originalNum + 7;
+1 var originalNumber = 23;
+2 var newNumber = originalNumber + 7;
+var 1stPresident = "Washington";
+var prezWhoCame1st = "Washington";
+alert(144);
+1 var caseQty = 144;
+2 alert(caseQty);
+
+// chapter 4th "Variable Names Legal and Illegal"
+userResponse
+userResponseTime
+userResponseTimeLimit
+response
+
+// chapter 5th "Math expressions:Familiar operators"
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+alert(2 + 2);
+var popularNumber = 12 - 24;
+var popularNumber = 3 * 12;
+1 var num = 10;
+2 var popularNumber = num + 200;
+1 var num = 10;
+2 var anotherNum = 1;
+3 var popularNumber = num + anotherNum;
+var whatsLeftOver = 10 % 3;
+var whatsLeftOver = 9 % 3;
+
+// chapter 6th "Math expressions:Unfamiliar operators"
+num++;
+num = num + 1;
+num--;
+1 var num = 1;
+2 var newNum = num++;
+1 var num = 1;
+2 var newNum = ++num;
+1 var num = 1;
+2 var newNum = num--;
+1 var num = 1;
+2 var newNum = --num;
+
+// chapter 7th "Math expressions:Eliminating ambiguity"
+var totalCost = 1 + 3 * 4;
+var totalCost = 1 + (3 * 4);
+var totalCost = (1 + 3) * 4;
+var resultOfComputation = (2 * 4) * 4 + 2;
+resultOfComputation = ((2 * 4) * 4) + 2;
+resultOfComputation = (2 * 4) * (4 + 2);
+
+// chapter 8th "Concatenating text strings"
+alert("Thanks for your input!");
+1 var message = "Thanks for your input!";
+2 alert(message);
+alert("Thanks, " + userName + "!");
+1 var message = "Thanks, ";
+2 var banger = "!";
+3 alert(message + userName + banger);
+alert("Thanks, " + "Susan" + "!");
+1 var message = "Thanks, ";
+2 var userName = "Susan";
+3 var banger = "!";
+4 var customMess = message + userName + banger;
+5 alert(customMess);
+alert("2" + "2");
+alert("2 plus 2 equals " + 2 + 2);
+
+// chapter 9th "Prompts"
+var spec = prompt("Your species?", "human");
+1 var question = "Your species?";
+2 var defaultAnswer = "human";
+3 var spec = prompt(question, defaultAnswer);
+1 var numberOfCats = prompt("How many cats?");
+2 var tooManyCats = numberOfCats + 1;
+
+
+
+// 2nd Task( 1st day) Date 06/04/2023
+var zakat_percentage = 0.025
+var user_asset = + prompt("Asset value")
+var result = zakat_percentage * user_asset
+alert ("Your zakat is "+ result )
